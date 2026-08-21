eee7d02c487bfe4266bf1d50dbb7d98e216c0332
Nithes <nithesvaran77@gmail.com>
2026-08-21 14:24:45 +0530

    Update 04_ReverseStack.java

--- 04_ReverseStack.java
+++ 04_ReverseStack.java
@@ -1,7 +1,23 @@
-import java.util.*;
-public class Program04 {
-    public static void main(String[] args){
-        // TODO: Implement ReverseStack
-        System.out.println("ReverseStack program");
+
+import java.util.Stack;
+
+public class P01_ReverseStringStack {
+    public static String reverseString(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reverseString(str));
     }
 }
